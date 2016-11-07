f4960b8fa9d95ca36ba2d8356315e6e004865264
Martijn-Sharp <martijn.heurter@gmail.com>
2016-11-07 10:30:51 +0100

    Added documentation

--- src/Incentro.Segona.Core/SegonaClient.cs
+++ src/Incentro.Segona.Core/SegonaClient.cs
@@ -28,31 +28,62 @@ namespace Incentro.Segona.Core
 
         protected string ApiKey { get; set; }
 
+        /// <summary>
+        /// Segona uses API keys to allow access to the API. 
+        /// You can register a new Segona API key at our developer portal (https://segona.io/).
+        /// </summary>
         public async Task<SegonaResponse<Keycheck>> ApiKeyCheckAsync()
         {
             return await Handler.ApiKeyCheckAsync(ApiKey);
         }
 
+        /// <summary>Retrieves all assets</summary>
+        /// <param name="limit">How many items you want to retrieve in a single call.</param>
+        /// <param name="startFromId">If supplied, results start from here. This is useful for pagination.</param>
         public async Task<SegonaResponse<AssetList>> GetAllAssetsAsync(int limit = 50, int? startFromId = null)
         {
             return await Handler.GetAllAssetsAsync(ApiKey, limit, startFromId);
         }
 
+        /// <summary>Retrieves a specific asset</summary>
+        /// <param name="id">The ID of the asset to retrieve</param>
         public async Task<SegonaResponse<AssetDetail>> GetSpecificAssetAsync(Guid id)
         {
             return await Handler.GetSpecificAssetAsync(ApiKey, id);
         }
 
+        /// <summary>Searches all assets</summary>
+        /// <param name="query">Your search term. Matches with labels, metadata, colors and the original file name</param>
+        /// <param name="limit">How many items you want to retrieve in a single call.</param>
         public async Task<SegonaResponse<AssetList>> SearchAssets(string query, int limit = 50)
         {
             return await Handler.SearchAssets(ApiKey, query, limit);
         }
 
+        /// <summary>Filters through assets</summary>
+        /// <param name="query">Your search term. Matches with labels, metadata, colors and the original file name</param>
+        /// <param name="limit">How many items you want to retrieve in a single call.</param>
+        /// <param name="extraQuery">An extra query term which does an “AND” with the query parameter.</param>
+        /// <param name="color">
+        /// Either a 6 digit hexcode (e.g. #FFFFFF) or one of these strings:
+        /// * Red
+        /// * Blue
+        /// * Green
+        /// * Black
+        /// * White
+        /// * Orange
+        /// * Grey
+        /// * Yellow
+        /// * Brown
+        /// * Purple
+        /// * Pink
+        /// </param>
         public async Task<SegonaResponse<AssetList>> FilteredSearchAssets(string query, int limit = 50, string extraQuery = null, string color = null)
         {
             return await Handler.FilteredSearchAssets(ApiKey, query, limit, extraQuery, color);
         }
 
+        /// <summary>Retrieves an upload url where you can upload your assets to</summary>
         public async Task<SegonaResponse<UploadUrlObject>> GetUploadUrl()
         {
             return await Handler.GetUploadUrl(ApiKey);
